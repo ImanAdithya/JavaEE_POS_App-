691111996ee0e66534413d4ee36c8e511ac89022
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-25 18:49:22 +0530

    first commit

---
+++ Back_End/src/lk/ijse/jsp/servlet/CustomerServlet.java
@@ -0,0 +1,54 @@
+package lk.ijse.jsp.servlet;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(urlPatterns = {"/pages/customer"})
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName ("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement ("select * from Customer");
+            ResultSet rst = pstm.executeQuery ();
+            PrintWriter writer = resp.getWriter ();
+            resp.addHeader ("Content-Type", "application/json");
+            resp.addHeader ("Access-Control-Allow-Origin", "*");
+
+            JsonArrayBuilder allCustomer = Json.createArrayBuilder ();
+
+
+            while (rst.next ()) {
+                String id = rst.getString (1);
+                String name = rst.getString (2);
+                String address = rst.getString (3);
+                String salary = (rst.getString (4));
+
+                JsonObjectBuilder customer = Json.createObjectBuilder ();
+
+                customer.add ("id", id);
+                customer.add ("name", name);
+                customer.add ("address", address);
+                customer.add ("salary", salary);
+
+                allCustomer.add (customer.build ());
+            }
+            writer.print (allCustomer.build ());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException (e);
+        } catch (SQLException e) {
+            throw new RuntimeException (e);
+        }
+    }
+}
