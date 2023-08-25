cd053cea6683c2517ded26df09fa394485b5e8db
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-25 21:06:47 +0530

    content updated

---
+++ Back_End/src/lk/ijse/jsp/servlet/ItemServlet.java
@@ -0,0 +1,63 @@
+package lk.ijse.jsp.servlet;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(urlPatterns = {"/itemPos"})
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName ("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement ("select * from item");
+            ResultSet rst = pstm.executeQuery ();
+            PrintWriter writer = resp.getWriter ();
+            resp.addHeader ("Content-Type", "application/json");
+            resp.addHeader ("Access-Control-Allow-Origin", "*");
+
+            JsonArrayBuilder allItem = Json.createArrayBuilder ();
+
+            while (rst.next ()) {
+                String id = rst.getString (1);
+                String des = rst.getString (2);
+                String up = rst.getString (3);
+                String qty = (rst.getString (4));
+
+                JsonObjectBuilder item = Json.createObjectBuilder ();
+
+                item.add ("itemId", id);
+                item.add ("itemDes", des);
+                item.add ("itemUp", up);
+                item.add ("itemQty", qty);
+
+                allItem.add (item.build ());
+            }
+            writer.print (allItem.build ());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException (e);
+        } catch (SQLException e) {
+            throw new RuntimeException (e);
+        }
+
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //preflight
+        resp.addHeader ("Access-Control-Allow-Origin", "*");
+        resp.addHeader ("Access-Control-Allow-Methods", "PUT");
+        resp.addHeader ("Access-Control-Allow-Methods", "DELETE");
+        resp.addHeader ("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
