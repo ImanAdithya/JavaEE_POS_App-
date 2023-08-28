071b75017844435153221ac471340cec90d0d768
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-28 09:07:00 +0530

    content updated

--- Back_End/src/lk/ijse/jsp/servlet/PlaceOrderServletAPI.java
+++ Back_End/src/lk/ijse/jsp/servlet/PlaceOrderServletAPI.java
@@ -1,10 +1,18 @@
 package lk.ijse.jsp.servlet;
 
+
+import javax.json.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(urlPatterns = "/placeOrder")
-public class PurchaseOrderServletAPI extends HttpServlet {
+public class PlaceOrderServletAPI extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
